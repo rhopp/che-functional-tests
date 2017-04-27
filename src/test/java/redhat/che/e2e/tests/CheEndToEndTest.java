b4c3c20b75f91093cdb763a13c4876044d652e4c
Matous Jobanek <mjobanek@redhat.com>
2017-04-27 13:43:34 +0200

    Added Arquillian Drone & Graphene

--- src/test/java/redhat/che/e2e/tests/CheEndToEndTest.java
+++ src/test/java/redhat/che/e2e/tests/CheEndToEndTest.java
@@ -10,6 +10,22 @@
 */
 package redhat.che.e2e.tests;
 
+import org.apache.log4j.Logger;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import redhat.che.e2e.tests.provider.CheWorkspaceProvider;
+import redhat.che.e2e.tests.resource.CheWorkspace;
+import redhat.che.e2e.tests.resource.CheWorkspaceStatus;
+import redhat.che.e2e.tests.selenium.ide.Labels;
+import redhat.che.e2e.tests.selenium.ide.Popup;
+import redhat.che.e2e.tests.selenium.ide.ProjectExplorer;
+import redhat.che.e2e.tests.selenium.ide.TestResultsView;
+import redhat.che.e2e.tests.service.CheWorkspaceService;
+
 import static redhat.che.e2e.tests.Constants.CHE_STARTER_URL;
 import static redhat.che.e2e.tests.Constants.CREATE_WORKSPACE_REQUEST_JSON;
 import static redhat.che.e2e.tests.Constants.KEYCLOAK_TOKEN;
@@ -19,69 +35,35 @@ import static redhat.che.e2e.tests.Constants.PATH_TO_TEST_FILE;
 import static redhat.che.e2e.tests.Constants.PRESERVE_WORKSPACE_PROPERTY_NAME;
 import static redhat.che.e2e.tests.Constants.PROJECT_NAME;
 
-import org.apache.log4j.Logger;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.junit.runners.MethodSorters;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import redhat.che.e2e.tests.provider.CheWorkspaceProvider;
-import redhat.che.e2e.tests.resource.CheWorkspace;
-import redhat.che.e2e.tests.resource.CheWorkspaceStatus;
-import redhat.che.e2e.tests.selenium.SeleniumProvider;
-import redhat.che.e2e.tests.selenium.ide.Labels;
-import redhat.che.e2e.tests.selenium.ide.Popup;
-import redhat.che.e2e.tests.selenium.ide.ProjectExplorer;
-import redhat.che.e2e.tests.selenium.ide.TestResultsView;
-import redhat.che.e2e.tests.service.CheWorkspaceService;
-
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@RunWith(JUnit4.class)
+@RunWith(Arquillian.class)
 public class CheEndToEndTest {
 
 	private static final Logger logger = Logger.getLogger(CheEndToEndTest.class);
-	
-	private static WebDriver driver;
-	private static ChromeDriverService chromeService;
+
+	@Drone
+	private WebDriver driver;
 	
 	private static CheWorkspace workspace;
-
-	@BeforeClass
-	public static void setUp() {
-		SeleniumProvider.setUpSeleniumChromeDriver();
-		chromeService = SeleniumProvider.startChromeDriverService();
-	}
 	
 	@Test
 	public void testCreateWorkspaceAndHandleProject() {
 		logger.info("Calling che starter to create a new workspace on OpenShift");
-		
-		workspace = CheWorkspaceProvider.createCheWorkspace(CHE_STARTER_URL, OPENSHIFT_MASTER_URL, 
+
+		workspace = CheWorkspaceProvider.createCheWorkspace(CHE_STARTER_URL, OPENSHIFT_MASTER_URL,
 				KEYCLOAK_TOKEN, CREATE_WORKSPACE_REQUEST_JSON, OPENSHIFT_NAMESPACE);
 		logger.info("Workspace successfully created.");
-		
+
 		logger.info("Waiting until workspace starts");
 		CheWorkspaceService.waitUntilWorkspaceGetsToState(workspace, CheWorkspaceStatus.RUNNING.getStatus());
-		
-		// Set web driver at the beginning of all Web UI tests
-		setWebDriver(workspace.getIdeLink());
-		
+
+		driver.get(workspace.getIdeLink());
 		// Running single JUnit Class
 		logger.info("Running JUnit test class on the project");
 		runTest(PROJECT_NAME);
 		checkTestResults();
-		
-		closeWebDriver();
 	}
 
-	private static void runTest(String projectName) {
+	private void runTest(String projectName) {
 		ProjectExplorer explorer = new ProjectExplorer(driver);
 		explorer.selectItem(PATH_TO_TEST_FILE);		
 		explorer.openContextMenuOnItem(PATH_TO_TEST_FILE);
@@ -93,25 +75,11 @@ public class CheEndToEndTest {
         testsPopup.waitWhileExists(Popup.RUNNING_TESTS_TITLE, 100);
         testsPopup.waitUntilExists(Popup.SUCCESSFULL_TESTS_TITLE, 10);
 	}
-	
-	private static void checkTestResults() {
+
+	private void checkTestResults() {
 		TestResultsView testView = new TestResultsView(driver);
 		testView.open();
 		testView.assertLatestTestRunPassed();
-	}
-	
-	private static void setWebDriver(String URL) {
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		capabilities.setCapability("networkConnectionEnabled", "true");
-		driver = new RemoteWebDriver(chromeService.getUrl(), capabilities);
-		driver.navigate().to(URL);
-	}
-	
-	private static void closeWebDriver() {
-		if (driver != null) {
-			driver.quit();
-			driver = null;
-		}
 	}
 	
 	private static boolean shouldNotDeleteWorkspace() {
@@ -128,17 +96,6 @@ public class CheEndToEndTest {
 	
 	@AfterClass
 	public static void cleanUp() {
-		if (driver != null) {
-			try {
-				driver.quit();
-			} catch (Exception ex) { 
-				// if something went wrong and driver couldnt quit, mostly bcs it is disposed
-				logger.info("Driver could not be disposed. Probably it is already disposed.");
-			}
-		}
-		if (chromeService != null && chromeService.isRunning()) {
-			SeleniumProvider.stopChromeDriverService(chromeService);
-		}
 		if (workspace != null && !shouldNotDeleteWorkspace()) {
 			if (CheWorkspaceService.getWorkspaceStatus(workspace).equals(CheWorkspaceStatus.RUNNING.getStatus())) {
 				logger.info("Stopping workspace");
