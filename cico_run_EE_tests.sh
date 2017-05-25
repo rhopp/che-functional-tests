a32f17fd7632626a83a8ca54d196c585bd934a23
mlabuda <marian.labuda@gmail.com>
2017-05-25 09:32:51 +0200

    Added check to keycloak token liveness before proceeding to tests

--- cico_run_EE_tests.sh
+++ cico_run_EE_tests.sh
@@ -47,7 +47,14 @@ if [[ ! -z "${empty_credentials}" ]]; then
   echo ${empty_credentials}
   exit 1
 else
-  echo 'OpenShift username and password and Keycloak token are not empty. Proceeding with EE tests.'
+  echo 'OpenShift username and password and Keycloak token are not empty.'
+fi
+if [[ $(curl -X GET -H "Authorization: Bearer ${KEYCLOAK_TOKEN}" https://sso.openshift.io/auth/realms/fabric8/broker/openshift-v3/token \
+   |  grep access_token | wc -l) -ne 1 ]]; then
+  echo "Keycloak token is expired"
+  exit 1
+else
+  echo "Keycloak token is alive. Proceeding with EE tests."
 fi
 echo 'export OSIO_USERNAME='${OSIO_USERNAME} >> ./env-vars
 echo 'export OSIO_PASSWORD='${OSIO_PASSWORD} >> ./env-vars
