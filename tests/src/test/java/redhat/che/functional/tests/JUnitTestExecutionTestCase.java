05093f945dd566322ec08d62252f91ff988ed377
kkanova <kkanova@redhat.com>
2017-09-13 09:50:26 +0200

    Hiding test functionality except GitTestCase

--- tests/src/test/java/redhat/che/functional/tests/JUnitTestExecutionTestCase.java
+++ tests/src/test/java/redhat/che/functional/tests/JUnitTestExecutionTestCase.java
@@ -1,47 +1,47 @@
-package redhat.che.functional.tests;
-
-import static redhat.che.functional.tests.utils.Constants.TEST_FILE;
-
-import org.jboss.arquillian.graphene.findby.FindByJQuery;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.support.FindBy;
-
-import redhat.che.functional.tests.fragments.ProjectItem;
-import redhat.che.functional.tests.fragments.TestResultsView;
-import redhat.che.functional.tests.fragments.popup.ContextMenu;
-import redhat.che.functional.tests.fragments.popup.Popup;
-
-@RunWith(Arquillian.class)
-public class JUnitTestExecutionTestCase extends AbstractCheFunctionalTest {
-
-    @FindBy(id = "menu-lock-layer-id")
-    private ContextMenu contextMenu;
-
-    @FindByJQuery("div#gwt-debug-popup-container")
-    private Popup popup;
-
-    @FindByJQuery("div#gwt-debug-perspectivePanel")
-    private TestResultsView testResultsView;
-
-    @Test
-    @InSequence(1)
-    public void test_run_junit_test_and_verify_popup_window() {
-        openBrowser();
-
-        ProjectItem testFile = project.getResource("src").getResource("test").getResource("java").getResource("io/openshift/booster")
-            .getResource(TEST_FILE);
-        testFile.openContextMenu();
-
-        contextMenu.selectRunJUnitClassTest();
-
-        popup.waitUntilTestsAreRunnig();
-        popup.waitWhileTestsAreRunning();
-        popup.waitUntilTestsAreFinishedSuccessfully();
-
-        testResultsView.open();
-        testResultsView.waitUntilTestsPassed();
-    }
-}
+//package redhat.che.functional.tests;
+//
+//import static redhat.che.functional.tests.utils.Constants.TEST_FILE;
+//
+//import org.jboss.arquillian.graphene.findby.FindByJQuery;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.arquillian.junit.InSequence;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.openqa.selenium.support.FindBy;
+//
+//import redhat.che.functional.tests.fragments.ProjectItem;
+//import redhat.che.functional.tests.fragments.TestResultsView;
+//import redhat.che.functional.tests.fragments.popup.ContextMenu;
+//import redhat.che.functional.tests.fragments.popup.Popup;
+//
+//@RunWith(Arquillian.class)
+//public class JUnitTestExecutionTestCase extends AbstractCheFunctionalTest {
+//
+//    @FindBy(id = "menu-lock-layer-id")
+//    private ContextMenu contextMenu;
+//
+//    @FindByJQuery("div#gwt-debug-popup-container")
+//    private Popup popup;
+//
+//    @FindByJQuery("div#gwt-debug-perspectivePanel")
+//    private TestResultsView testResultsView;
+//
+////    @Test
+//    @InSequence(1)
+//    public void test_run_junit_test_and_verify_popup_window() {
+//        openBrowser();
+//
+//        ProjectItem testFile = project.getResource("src").getResource("test").getResource("java").getResource("io/openshift/booster")
+//            .getResource(TEST_FILE);
+//        testFile.openContextMenu();
+//
+//        contextMenu.selectRunJUnitClassTest();
+//
+//        popup.waitUntilTestsAreRunnig();
+//        popup.waitWhileTestsAreRunning();
+//        popup.waitUntilTestsAreFinishedSuccessfully();
+//
+//        testResultsView.open();
+//        testResultsView.waitUntilTestsPassed();
+//    }
+//}
--- tests/src/test/java/redhat/che/functional/tests/PomTestCase.java
+++ tests/src/test/java/redhat/che/functional/tests/PomTestCase.java
@@ -1,23 +1,26 @@
-package redhat.che.functional.tests;
-
-import org.jboss.arquillian.junit.Arquillian;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.support.FindBy;
-
-import redhat.che.functional.tests.fragments.CodeEditorFragment;
-
-@RunWith(Arquillian.class)
-public class PomTestCase extends AbstractCheFunctionalTest {
-
-    @FindBy(id = "gwt-debug-editorPartStack-contentPanel")
-    private CodeEditorFragment codeEditor;
-
-    @Test
-    public void test_when_pom_has_older_dependency_version_annotation_error_is_shown() {
-        openBrowser();
-        project.getResource("pom.xml").open();
-        codeEditor.writeDependencyIntoPom();
-        codeEditor.verifyAnnotationErrorIsPresent();
-    }
-}
+//package redhat.che.functional.tests;
+//
+//import static org.junit.Assert.*;
+//
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.openqa.selenium.support.FindBy;
+//
+//import redhat.che.functional.tests.fragments.CodeEditorFragment;
+//
+//@RunWith(Arquillian.class)
+//public class PomTestCase extends AbstractCheFunctionalTest {
+//
+//    @FindBy(id = "gwt-debug-editorPartStack-contentPanel")
+//    private CodeEditorFragment codeEditor;
+//
+//    @Test
+//    public void test_when_pom_has_older_dependency_version_annotation_error_is_shown() {
+//        openBrowser();
+//        project.getResource("pom.xml").open();
+//        codeEditor.writeDependencyIntoPom();
+//        codeEditor.verifyAnnotationErrorIsPresent();
+//        fail();
+//    }
+//}
