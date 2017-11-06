c38784f7d1f57e3686853f8627328ccd2751240c
rhopp <rhopp@redhat.com>
2017-11-06 16:06:42 +0100

    Updated che-selenium docker image.
    Signed-off-by: rhopp <rhopp@redhat.com>

--- cico_run_EE_tests.sh
+++ cico_run_EE_tests.sh
@@ -24,7 +24,7 @@ source update_tenant.sh
 # Run test image
 cat /tmp/jenkins-env >> ./env-vars
 chown -R 1000:1000 ./*
-docker run -d --user=fabric8 --cap-add SYS_ADMIN --name=che-selenium -t -v $(pwd):/home/fabric8/che:Z mlabuda/che-selenium:170621
+docker run -d --user=fabric8 --cap-add SYS_ADMIN --name=che-selenium -t -v $(pwd):/home/fabric8/che:Z rhopp/che-selenium:latest
 
 ## Exec tests
 docker exec --user=fabric8 che-selenium /home/fabric8/che/run_EE_tests.sh
