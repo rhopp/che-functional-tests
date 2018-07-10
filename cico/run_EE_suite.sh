6127406d8019ef73b223c5cdc251017ebab8159c
kkanova <kkanova@redhat.com>
2018-07-10 11:52:32 +0200

    Don't delete workspace when test ends.

--- cico/run_EE_suite.sh
+++ cico/run_EE_suite.sh
@@ -1,4 +1,4 @@
 #osioUrlPart should be "openshift.io" or "prod-preview.openshift.io"
 #cheWorkspaceName is name displayed in dashboard - not and ID!
 mvn clean install -f pom.xml -DskipTests
-mvn clean verify -f tests/pom.xml -Dtest=E2ETestSuite -DosioUrlPart=$1 -DosioUsername=$2 -DosioPassword=$3 -DcheWorkspaceName=$4 -DkeycloakToken=$5
+mvn clean verify -f tests/pom.xml -Dtest=E2ETestSuite -DosioUrlPart=$1 -DosioUsername=$2 -DosioPassword=$3 -DcheWorkspaceName=$4 -DkeycloakToken=$5 -DpreserveWorkspace=true
