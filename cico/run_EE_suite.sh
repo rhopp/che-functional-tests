fe0d8f1a0d8d8027afbcc13a81f5975bc20c212c
kkanova <kkanova@redhat.com>
2018-07-11 12:12:58 +0200

    Add exit command to E2E tests.

--- cico/run_EE_suite.sh
+++ cico/run_EE_suite.sh
@@ -2,3 +2,5 @@
 #cheWorkspaceName is name displayed in dashboard - not and ID!
 mvn clean install -f pom.xml -DskipTests
 mvn clean verify -f tests/pom.xml -Dtest=E2ETestSuite -DosioUrlPart=$1 -DosioUsername=$2 -DosioPassword=$3 -DcheWorkspaceName=$4 -DkeycloakToken=$5 -DpreserveWorkspace=true
+
+exit $?
